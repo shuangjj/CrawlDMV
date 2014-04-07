10a219ce8957ac5325de60dba4314d9e951f1f3e
shuang-ux32vd <co.liang.ol@gmail.com>
2014-04-06 23:06:01 -0400

    add command line args for licence file and run on server

--- crawler.py
+++ crawler.py
@@ -10,13 +10,52 @@ from selenium.webdriver.common.keys import Keys
 import smtplib
 from email.mime.text import MIMEText
 
+import argparse
+from argparse import RawDescriptionHelpFormatter
+
+from pyvirtualdisplay import Display
 
 check_interval = 60*5
 
 def main():
     dmv_url = "http://www.dot4.state.pa.us/centers/OnlineServicesCenter.shtml"
     register_url = "https://www.dot4.state.pa.us/exam_scheduling/eslogin.jsp#top?20140405115120899=20140405115120899"
-    #print urllib.urlopen(register_url).read()
+    # alert
+    alert_msg = "Road Test Available at %s\n" % (time.asctime())
+    alert_msg = alert_msg + '''Register online at 'http://www.dot3.state.pa.us/centers/OnlineServicesCenter.shtml'
+Thank you!'''
+    alert_by_mail(alert_msg)
+    print alert_msg
+    sys.exit(0)
+
+    #
+    prompt = '''
+    +------------------------------------------------------+
+    | Crawl DMV for Road Test Registration.                |
+    | Written by somebody who failed the exam 4 times.     |
+    +------------------------------------------------------+
+    '''
+    parser = argparse.ArgumentParser(description=prompt, formatter_class=RawDescriptionHelpFormatter)
+    parser.add_argument('--server', action='store_true',
+                        help='Running the program on server with virtual display.')
+    parser.add_argument('--license', default='license.txt', 
+                        help="License file containing driver's number and date of birth on each line")
+    args = parser.parse_args()
+    
+    # Get login credentials from file
+    try:
+        fp = open(args.license, 'r')
+    except IOError:
+        print '%s not found!' % (args.license)
+        sys.exit()
+    else:
+        credentials = fp.readlines()
+        fp.close()
+    # Run on server
+    if args.server:
+        display = Display(visible=0, size=(800, 600))
+        display.start()
+
     ## Load ChromeDriver
     chromedriver = "/opt/drivers/chromedriver"
     os.environ["webdriver.chrome.driver"] = chromedriver
@@ -29,10 +68,10 @@ def main():
 
     # Setup driver number
     driverNum_element = driver.find_element_by_id("driverNum")
-    driverNum_element.send_keys("31433298")
+    driverNum_element.send_keys(credentials[0].strip())
     # Date of Birth
     dob_element = driver.find_element_by_id("dob")
-    dob_element.send_keys("12/01/1988")
+    dob_element.send_keys(credentials[1].strip())
     # Login
     continueButton = driver.find_element_by_name("continueButton")
     continueButton.click()
@@ -61,21 +100,27 @@ def main():
         findError = page_source.find("There are no exams available for the criteria that you selected.")
         if findError == -1: # Available time
             # alert
-            alert_by_mail("Road Test Available at " % (time.asctime()))
+            alert_msg = "Road Test Available at %s" % (time.asctime())
+            alert_msg.append('''Register online at 'http://www.dot3.state.pa.us/centers/OnlineServicesCenter.shtml'
+Thank you!''')
+            alert_by_mail(alert_msg)
+            print alert_msg
             sys.exit(0)
         else:
-            print "No exams, sleep!"
+            print "%s: No exams, sleep!" % (time.asctime())
             time.sleep(check_interval)
 
-def alert_by_mail(msg):
+def alert_by_mail(alert_msg):
     from_addr = "tue68607@temple.edu"
+    recipients = ['co.liang.ol@gmail.com', 'qcaisuda@gmail.com']
     to_addr = "co.liang.ol@gmail.com"
-    msg = MIMEText(msg)
-    msg['Subject'] = msg
+    
+    msg = MIMEText(alert_msg)
+    msg['Subject'] = "Road Test Available"
     msg['From'] = from_addr
-    msg['To'] = to_addr
+    msg['To'] = ', '.join(recipients)
     s = smtplib.SMTP('localhost')
-    s.sendmail(from_addr, to_addr, msg.as_string())
+    s.sendmail(from_addr, recipients, msg.as_string())
     s.quit()
 
 
